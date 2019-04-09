d4bcb917e58c5f4f500e7aba8d394aaa94a1cf31
Amory Aubert <amory.aubert@gmail.com>
2019-04-09 15:20:58 +0200

    etape 1 ok

--- index.php
+++ index.php
@@ -13,23 +13,25 @@ function todo(){
     $bdd = new PDO('mysql:host=localhost;dbname=Becode;charset=utf8', 'Amory', 'user');
     $data = $bdd->query("SELECT * FROM ToDoList WHERE DO='0'")->fetchAll();
     // and somewhere later:
-    foreach ($data as $row) {
-        echo "<p><input type='checkbox' name='DO[]' value='0' class='checkbox'>".$row['TASK']."</p>";
+    foreach ($data as $key => $row) {
+        echo "<p class='todop'><input type='checkbox' name='DO[]' value='".$row['ID']."' class='checkbox'>".$row['TASK']."</p>";
+    }
+}
+function isdo(){
+    $bdd = new PDO('mysql:host=localhost;dbname=Becode;charset=utf8', 'Amory', 'user');
+    $data = $bdd->query("SELECT * FROM ToDoList WHERE DO='1'")->fetchAll();
+    // and somewhere later:
+    foreach ($data as $key => $row) {
+        echo "<p class='isdop'><input type='checkbox' value='".$row['ID']."' class='checkbox' disabled='disabled' checked='checked'>".$row['TASK']."</p>";
     }
 }
 if(isset($_POST['afaire'])){
-    $DO=$_POST['DO'];
-    $N = count($DO);
-    for($i=0; $i < $N; $i++)
-    {
-        $DO[$i]='0';
-        $data = [
-            'DO' => $DO[$i],
-        ];
-        echo($DO[$i] . " ");
-        $sql = "UPDATE ToDoList SET DO=:DO WHERE DO!=:DO";
-        $bdd->prepare($sql)->execute($data);
-    }
+	$todelete = $_POST['DO']; 
+	$stmt = $bdd->prepare("UPDATE ToDoList SET DO='1' WHERE ID = :ID");
+	foreach ($todelete as $id)
+    $stmt->execute(array(":ID" => $id));
+	header('Location: index.php');
+    exit();
 }
 if(isset($_POST['ajout'])){
     $options = array(
@@ -46,8 +48,9 @@ if(isset($_POST['ajout'])){
     ];
     $sql = "INSERT INTO ToDoList (TASK, DO) VALUES (:TASK, :DO)";
     $bdd->prepare($sql)->execute($data);
+	header('Location: index.php');
+    exit();
 }
-//$bdd->close();
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -62,23 +65,26 @@ if(isset($_POST['ajout'])){
 </head>
 <body>
 <h1>ToDo List</h1>
-<form id="ToDo" method="post" action="#">
+<form id="ToDo" method="post" action="index.php">
     <fieldset class="border border-primary p-3 ToDo">
         <legend class="w-auto">A Faire</legend>
-        <div id="content">
+        <div id="afaire">
             <?php todo(); ?>
         </div>
         <input type="submit" value="Archiver" name="afaire">
     </fieldset>
     <fieldset class="border border-primary p-3">
         <legend class="w-auto">Archive</legend>
+		<div id="archive">
+            <?php isdo(); ?>
+        </div>
     </fieldset>
 </form>
-<form id="Add" method="post" action="#">
+<form id="Add" method="post" action="index.php">
     <fieldset class="border border-primary p-3">
         <legend class="w-auto">Ajouter</legend>
         <textarea name="task" rows="2" required class="col-md-9"></textarea>
-        <input type="submit" value="Ajouter" name="ajout" id="ajout">
+        <input type="submit" value="Ajouter" name="ajout" id="ajout" class="col-md-9">
     </fieldset>
 </form>
 </body>
